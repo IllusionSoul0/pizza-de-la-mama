57b2c61ec079156f247f87c125b116b410b7faf1
Maxime Chaintreau <130096294+IllusionSoul0@users.noreply.github.com>
2026-04-19 20:57:30 +0200

    Création du composant toast

--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -9,15 +9,21 @@
     <h3>Total : {{ total }} €</h3>
 
     <button @click="order">Commander</button>
+
+    <Toast v-if="showToast" :message="message" />
   </div>
 </template>
 
 <script setup>
 import { inject, ref, watch } from "vue";
 import CartItem from "@/components/CartItem.vue";
+import Toast from "@/components/Toast.vue";
 
 const cart = inject("cart");
 const orders = inject("orders");
+
+const showToast = ref(false);
+const message = ref("");
 
 const total = ref(cart.value.reduce((sum, dish) => (sum += dish.cost * dish.quantity), 0));
 
@@ -30,23 +36,34 @@ watch(
 );
 
 function order() {
-    if (cart.value.length === 0) {
-        //Add Error Toast
-        return;
-    }
+  if (cart.value.length === 0) {
+    message.value = "La commande est vide";
+    showToast.value = true;
+
+    setTimeout(() => {
+      showToast.value = false;
+    }, 2000);
+    return;
+  }
   orders.value.push({
     id: Date.now(),
     dishs: [...cart.value],
     ready: false,
   });
   cart.value = [];
+  message.value = "La commande a été validée";
+  showToast.value = true;
+
+  setTimeout(() => {
+    showToast.value = false;
+  }, 2000);
 }
 
 function changeAmount(dish, amount) {
   const dishInCart = cart.value.find((element) => element.id === dish.id);
   dishInCart.quantity += amount;
   if (dishInCart.quantity < 1) {
-    cart.value = cart.value.filter(element => element.id !== dishInCart.id)
+    cart.value = cart.value.filter((element) => element.id !== dishInCart.id);
   }
 }
 </script>
--- src/pages/MenuPage.vue
+++ src/pages/MenuPage.vue
@@ -4,15 +4,21 @@
     <DishItem v-for="dish in menu" :key="dish.id" :dish="dish" @addToCart="addToCart">
       <span v-if="getDishQuantity(dish.id) > 0">Dans le panier : {{ getDishQuantity(dish.id) }}</span>
     </DishItem>
+
+    <Toast v-if="showToast" :message="message" />
   </div>
 </template>
 
 <script setup>
 import menu from "@/data/menu";
 import DishItem from "@/components/DishItem.vue";
-import { inject } from "vue";
+import Toast from "@/components/Toast.vue";
+import { inject, ref } from "vue";
 
 const cart = inject("cart");
+
+const showToast = ref(false);
+const message = ref("");
 
 function addToCart(dish) {
   const exist = cart.value.find((element) => element.id === dish.id);
@@ -22,6 +28,13 @@ function addToCart(dish) {
   } else {
     cart.value.push({ ...dish, quantity: 1 });
   }
+
+  message.value = dish.name + " ajouté !";
+  showToast.value = true;
+
+  setTimeout(() => {
+    showToast.value = false;
+  }, 2000);
 }
 
 function getDishQuantity(id) {
