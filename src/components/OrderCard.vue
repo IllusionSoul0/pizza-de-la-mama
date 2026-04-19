e579fb6c59655e215e03126006268101e88af422
Maxime Chaintreau <130096294+IllusionSoul0@users.noreply.github.com>
2026-04-19 20:51:41 +0200

    Ajout de la page admin

---
+++ src/components/OrderCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>Commande {{ order.id }}</h3>
+
+    <p v-for="dish in order.dishs">{{ dish.name }} x{{ dish.quantity }}</p>
+
+    <button v-if="!order.ready" @click="order.ready = true">Marquer prête</button>
+
+    <p v-else>Prête</p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  order: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
--- src/pages/AdminPage.vue
+++ src/pages/AdminPage.vue
@@ -1,5 +1,16 @@
 <template>
+  <div>
+    <h2>Commandes</h2>
 
+    <p v-if="orders.length === 0">Aucune commande</p>
+
+    <OrderCard v-for="order in orders" :key="order.id" :order="order"></OrderCard>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { inject } from "vue";
+import OrderCard from "@/components/OrderCard.vue";
+
+const orders = inject("orders");
+</script>
